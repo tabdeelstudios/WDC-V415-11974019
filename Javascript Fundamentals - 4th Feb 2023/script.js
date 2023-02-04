f44289a27959578fe74cea3716ed4a558ca8c6b4
Aquib Ajani <aquib@tabdeel.in>
2023-02-04 13:05:52 +0530

    Add files via upload

---
+++ Javascript Fundamentals - 4th Feb 2023/script.js
@@ -0,0 +1,77 @@
+// Data Types
+// Number - Integers, Decimals
+// String - Character, Word, Phrase, Sentence, Paragraph
+// Boolean - true, false
+// null
+// undefined
+
+// Numbers - 100, 17, 900.89, 0, -100
+// String - 'a', 'Hello', "Hi there!", "THis is a sentence."
+// Boolean - true, false
+
+// Dynamically Typed Language
+// var isEmployed = true;
+// console.log(isEmployed);
+
+// Case sensitivity - Sensitive
+// let USERNAME = "JOHN";
+// let username = "john";
+// console.log(USERNAME);
+// console.log(username);
+
+// camelCase
+// userMobileNumber
+// userEmail
+// userStreetAddress
+
+// White space sensitivity - Insensisitive
+// let username = "John";
+
+// Activity:
+// 1. Document Edited By Last - documentEditedByLast
+// 2. User Home Location - userHomeLocation
+// 3. Email Sent To - emailSentTo
+// 4. Person Courier Details - personCourierDetails
+// 5. Final Order Price AFTER Discount - finalOrderPruceAfterDiscount
+
+// Underscore Casing
+// 1. user_email_id
+// 2. person_courier_details
+// 3. final_order_price_after_discount
+
+// let, var and const
+
+// Developer A
+// let billAmount = 120.56;
+// console.log(billAmount);
+
+// Developer B
+// let billAmount = 45.00;
+// console.log(billAmount);
+
+// const TAX_RATE = 0.05;
+// const USER_ID = 12345;
+
+// Decision Making
+
+// Ternary Operator
+
+// (condition) ? what happens if true : what happens if false;
+// let isUserLoggedIn = false;
+// isUserLoggedIn ? console.log("Show Logout") : console.log("Show Login");
+
+// If the billAmount is more than 2000, show 5% discount applied. Otherwise, show no discount applied
+// let billAmount = 2500.00;
+// billAmount > 2000.00 ? console.log("5% discount") : console.log("No discount");
+
+// if-else construct
+// let userName="xyz@gmail.com";
+// console.log(userName)=""
+
+// Machine code
+// Assembly language
+// High Level language
+
+// Compiler
+
+// Interpreter
