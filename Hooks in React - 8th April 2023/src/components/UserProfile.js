6c2035c325e87286f0c0bb5681c8ee2eeb1c6e26
Aquib Ajani <aquib@tabdeel.in>
2023-04-08 11:54:06 +0530

    Add files via upload

---
+++ Hooks in React - 8th April 2023/src/components/UserProfile.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function UserProfile() {
+  const [userData, setUserData] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    let loaded = true;
+    fetch("https://randomuser.me/api")
+      .then((res) => res.json())
+      .then((data) => {
+        if (loaded) {
+          setUserData(data);
+          setIsLoaded(true);
+        }
+      });
+    return () => (loaded = false);
+  }, []);
+
+  return (
+    <>
+      {isLoaded ? (
+        <div>
+          <img src={userData.results[0].picture.medium} />
+          <h1>{userData.results[0].name.first}</h1>
+          <p>{userData.results[0].email}</p>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
+  );
+}
+
+export default UserProfile;
--- Hooks in React - 8th April 2023/src/index.js
+++ Hooks in React - 8th April 2023/src/index.js
@@ -6,14 +6,16 @@ import reportWebVitals from "./reportWebVitals";
 import Counter from "./components/Counter";
 import Login from "./components/Login";
 import ToDoList from "./components/ToDoList";
+import UserProfile from "./components/UserProfile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Counter /> */}
     {/* <Login /> */}
     {/* <ToDoList /> */}
+    <UserProfile />
   </React.StrictMode>
 );
 
