e42295635552fbdc1e64e1512928b495f3bab5b1
Aquib Ajani <aquib@tabdeel.in>
2023-05-07 12:41:10 +0530

    Add files via upload

--- 7th May 2023 - Node, Express and React/backend/index.js
+++ 7th May 2023 - Node, Express and React/backend/index.js
@@ -16,39 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-var allTasks = [];
-
 var allProducts = [];
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello there!" });
-});
-
-// Show 5 tasks on the /tasks route. Each task returns an ID, title and status.
-app.get("/tasks", (req, res) => {
-  res.json({
-    tasks: [
-      { id: 1, title: "Buy milk", status: "complete" },
-      { id: 2, title: "Send email", status: "complete" },
-      { id: 3, title: "Attend meeting", status: "complete" },
-      { id: 4, title: "Plan the day", status: "complete" },
-      { id: 5, title: "Watch a movie", status: "complete" },
-    ],
-  });
-});
-
-app.post("/addTask", (req, res) => {
-  let task = req.body;
-  allTasks.push(task);
-  res.json({ message: "Your task " + task.title + " is saved!" });
-});
-
-app.get("/allTasks", (req, res) => {
-  res.json({ allTasks: allTasks });
-});
-// app.get("/addTask", (req, res) => {
-//   res.send("Your task is saved! All tasks are : " + allTasks);
-// });
 
 app.post("/addProduct", (req, res) => {
   let data = req.body;
@@ -61,6 +29,41 @@ app.get("/addProduct", (req, res) => {
   res.json({ products: allProducts });
 });
 
+app.delete("/addProduct", (req, res) => {
+  let data = allProducts.shift();
+  res.json({ message: "Product " + data.name + " deleted succesfully!" });
+});
+
 app.listen(port, () => {
   console.log("Server is running!");
 });
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello there!" });
+// });
+
+// Show 5 tasks on the /tasks route. Each task returns an ID, title and status.
+// app.get("/tasks", (req, res) => {
+//   res.json({
+//     tasks: [
+//       { id: 1, title: "Buy milk", status: "complete" },
+//       { id: 2, title: "Send email", status: "complete" },
+//       { id: 3, title: "Attend meeting", status: "complete" },
+//       { id: 4, title: "Plan the day", status: "complete" },
+//       { id: 5, title: "Watch a movie", status: "complete" },
+//     ],
+//   });
+// });
+
+// app.post("/addTask", (req, res) => {
+//   let task = req.body;
+//   allTasks.push(task);
+//   res.json({ message: "Your task " + task.title + " is saved!" });
+// });
+
+// app.get("/allTasks", (req, res) => {
+//   res.json({ allTasks: allTasks });
+// });
+// app.get("/addTask", (req, res) => {
+//   res.send("Your task is saved! All tasks are : " + allTasks);
+// });
