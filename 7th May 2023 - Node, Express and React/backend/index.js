c9a728008fff052409a599ea42f64d7c888bd959
Aquib Ajani <aquib@tabdeel.in>
2023-05-07 12:17:28 +0530

    Add files via upload

--- 7th May 2023 - Node, Express and React/backend/index.js
+++ 7th May 2023 - Node, Express and React/backend/index.js
@@ -16,6 +16,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+var allTasks = [];
+
+var allProducts = [];
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello there!" });
 });
@@ -31,6 +35,30 @@ app.get("/tasks", (req, res) => {
       { id: 5, title: "Watch a movie", status: "complete" },
     ],
   });
+});
+
+app.post("/addTask", (req, res) => {
+  let task = req.body;
+  allTasks.push(task);
+  res.json({ message: "Your task " + task.title + " is saved!" });
+});
+
+app.get("/allTasks", (req, res) => {
+  res.json({ allTasks: allTasks });
+});
+// app.get("/addTask", (req, res) => {
+//   res.send("Your task is saved! All tasks are : " + allTasks);
+// });
+
+app.post("/addProduct", (req, res) => {
+  let data = req.body;
+  allProducts.push(data);
+  res.json({ message: "Product " + data.name + " saved succesfully!" });
+  console.log(allProducts);
+});
+
+app.get("/addProduct", (req, res) => {
+  res.json({ products: allProducts });
 });
 
 app.listen(port, () => {
